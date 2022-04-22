b914b0574cfc32b6aa602691dc9d8d6ab42f1f1f
masilmam <ilmam.javajin@gmail.com>
2022-04-22 08:46:28 +0700

    Create register view

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -6,14 +6,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Guest Book System - Login</title>
+  <title>Guestbook System - Login</title>
 
   <!-- Custom fonts for this template-->
   <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
@@ -64,7 +63,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                       <a class="small" href="#">Forgot Password?</a>
                     </div>
                     <div class="text-center">
-                      <a class="small" href="#">Create an Account!</a>
+                      <a class="small" href="<?= base_url('Auth/register'); ?>">Create an Account!</a>
                     </div>
                 </div>
               </div>
